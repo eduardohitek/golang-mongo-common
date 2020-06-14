9b268c2f26d7dac9223245b0d07457c75e7ee953
Eduardo Hitek <eduardohitek@gmail.com>
2020-06-14 13:44:22 -0300

    Metodo de conexão no Mongo Atlas e organização das funcs

--- common.go
+++ common.go
@@ -1,7 +1,8 @@
-package common
+package main
 
 import (
 	"context"
+	"fmt"
 	"log"
 	"reflect"
 	"time"
@@ -11,62 +12,91 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-func RetornarCliente(url string, appName string) (*mongo.Client, error) {
+//DBCommon represents a DBCommon Object
+type DBCommon struct {
+	Log *log.Logger
+}
+
+//NewDBCommon constructor
+func NewDBCommon(log *log.Logger) *DBCommon {
+	return &DBCommon{Log: log}
+}
+
+func (dbc *DBCommon) retornarCliente(url string, appName string) (*mongo.Client, error) {
 	clientOptions := options.Client().ApplyURI("mongodb://" + url).SetConnectTimeout(10 * time.Second)
 	clientOptions.SetAppName(appName)
 	clientOptions.SetMaxConnIdleTime(15 * time.Second)
 	clientOptions.SetServerSelectionTimeout(10 * time.Second)
 	client, erro := mongo.NewClient(clientOptions)
 	if erro != nil {
-		log.Fatal(erro)
+		dbc.Log.Fatal("Erro ao criar o cliente de conexão com o DB", erro.Error())
 		return nil, erro
 	}
 	erro = client.Connect(context.Background())
 	if erro != nil {
-		log.Fatal(erro)
+		dbc.Log.Fatal("Erro ao se conectar com o DB", erro.Error())
 		return nil, erro
 	}
 	return client, nil
 }
 
-func RetornarClienteSeguro(url string, authDB string, user string, password string, appName string) (*mongo.Client, error) {
+func (dbc *DBCommon) retornarClienteSeguro(url string, authDB string, user string, password string, appName string) (*mongo.Client, error) {
 	credentials := options.Credential{AuthSource: authDB, Username: user, Password: password}
 	connectionOptions := options.Client().ApplyURI("mongodb://" + url).SetAppName(appName).SetAuth(credentials).SetConnectTimeout(5 * time.Second)
 	connectionOptions.SetMaxConnIdleTime(15 * time.Second)
 	connectionOptions.SetServerSelectionTimeout(10 * time.Second)
 	client, erro := mongo.NewClient(connectionOptions)
 	if erro != nil {
-		log.Fatal("Erro ao efetuar conexão com o DB", erro.Error())
+		dbc.Log.Fatal("Erro ao criar o cliente de conexão com o DB", erro.Error())
 		return nil, erro
 	}
 	erro = client.Connect(context.Background())
 	if erro != nil {
-		log.Fatal("Erro ao efetuar conexão com o DB", erro.Error())
+		dbc.Log.Fatal("Erro ao se conectar com o DB", erro.Error())
 		return nil, erro
 	}
 	return client, nil
 }
 
-func Total(nomeDB string, nomeColecao string, client *mongo.Client, filtro interface{}) (int64, error) {
+func (dbc *DBCommon) retornarClienteSeguroMongoAtlas(url string, user string, password string, db string, appName string) (*mongo.Client, error) {
+	connectionURL := fmt.Sprintf("mongodb+srv://%s:%s@tu-8pfjk.mongodb.net/%s?retryWrites=true&w=majority", user, password, db)
+	connectionOptions := options.Client().ApplyURI(connectionURL)
+	connectionOptions.SetAppName(appName).SetConnectTimeout(10 * time.Second)
+	connectionOptions.SetMaxConnIdleTime(15 * time.Second)
+	connectionOptions.SetServerSelectionTimeout(10 * time.Second)
+	client, erro := mongo.NewClient(connectionOptions)
+	if erro != nil {
+		dbc.Log.Fatal("Erro ao criar o cliente de conexão com o DB", erro.Error())
+		return nil, erro
+	}
+	erro = client.Connect(context.Background())
+	if erro != nil {
+		dbc.Log.Fatal("Erro ao se conectar com o DB", erro.Error())
+		return nil, erro
+	}
+	return client, nil
+}
+
+func (dbc *DBCommon) total(nomeDB string, nomeColecao string, client *mongo.Client, filtro interface{}) (int64, error) {
 	collection := client.Database(nomeDB).Collection(nomeColecao)
 	total, erro := collection.CountDocuments(context.TODO(), filtro)
 	return total, erro
 }
 
-func DeletarPeloId(nomeDB string, nomeColecao string, client *mongo.Client, insertedID interface{}) (*mongo.DeleteResult, error) {
+func (dbc *DBCommon) deletarPeloID(nomeDB string, nomeColecao string, client *mongo.Client, insertedID interface{}) (*mongo.DeleteResult, error) {
 	collection := client.Database(nomeDB).Collection(nomeColecao)
 	filtro := bson.M{"_id": insertedID}
 	deleteResult, erro := collection.DeleteOne(context.TODO(), filtro)
 	return deleteResult, erro
 }
 
-func Deletar(nomeDB string, nomeColecao string, client *mongo.Client, filtro interface{}) (*mongo.DeleteResult, error) {
+func (dbc *DBCommon) deletar(nomeDB string, nomeColecao string, client *mongo.Client, filtro interface{}) (*mongo.DeleteResult, error) {
 	collection := client.Database(nomeDB).Collection(nomeColecao)
 	deleteResult, erro := collection.DeleteOne(context.TODO(), filtro)
 	return deleteResult, erro
 }
 
-func AtualizarPeloId(nomeDB string, nomeColecao string, client *mongo.Client, insertedID interface{}, campoAtualizado interface{}) (*mongo.UpdateResult, error) {
+func (dbc *DBCommon) atualizarPeloID(nomeDB string, nomeColecao string, client *mongo.Client, insertedID interface{}, campoAtualizado interface{}) (*mongo.UpdateResult, error) {
 	collection := client.Database(nomeDB).Collection(nomeColecao)
 	atualizacao := bson.D{{Key: "$set", Value: campoAtualizado}}
 	filtro := bson.M{"_id": insertedID}
@@ -74,27 +104,27 @@ func AtualizarPeloId(nomeDB string, nomeColecao string, client *mongo.Client, in
 	return updateResult, erro
 }
 
-func Atualizar(nomeDB string, nomeColecao string, client *mongo.Client, filtro interface{}, campoAtualizado interface{}) (*mongo.UpdateResult, error) {
+func (dbc *DBCommon) atualizar(nomeDB string, nomeColecao string, client *mongo.Client, filtro interface{}, campoAtualizado interface{}) (*mongo.UpdateResult, error) {
 	collection := client.Database(nomeDB).Collection(nomeColecao)
 	atualizacao := bson.D{{Key: "$set", Value: campoAtualizado}}
 	updateResult, erro := collection.UpdateOne(context.TODO(), filtro, atualizacao)
 	return updateResult, erro
 }
 
-func Adicionar(ctx context.Context, nomeDB string, nomeColecao string, documento interface{}, client *mongo.Client) (*mongo.InsertOneResult, error) {
+func (dbc *DBCommon) adicionar(ctx context.Context, nomeDB string, nomeColecao string, documento interface{}, client *mongo.Client) (*mongo.InsertOneResult, error) {
 	collection := client.Database(nomeDB).Collection(nomeColecao)
 	c := context.TODO()
 	insertOneResult, erro := collection.InsertOne(c, documento)
 	return insertOneResult, erro
 }
 
-func AdicionarVarios(tx context.Context, nomeDB string, nomeColecao string, documentos []interface{}, client *mongo.Client) (*mongo.InsertManyResult, error) {
+func (dbc *DBCommon) adicionarVarios(tx context.Context, nomeDB string, nomeColecao string, documentos []interface{}, client *mongo.Client) (*mongo.InsertManyResult, error) {
 	collection := client.Database(nomeDB).Collection(nomeColecao)
 	insertManyResult, erro := collection.InsertMany(context.TODO(), documentos)
 	return insertManyResult, erro
 }
 
-func RetornarUm(nomeDB string, nomeColecao string, modelo interface{}, client *mongo.Client,
+func (dbc *DBCommon) retornarUm(nomeDB string, nomeColecao string, modelo interface{}, client *mongo.Client,
 	filtro bson.M, findOption *options.FindOneOptions) error {
 	collection := client.Database(nomeDB).Collection(nomeColecao)
 	a := collection.FindOne(context.TODO(), filtro, findOption)
@@ -102,7 +132,7 @@ func RetornarUm(nomeDB string, nomeColecao string, modelo interface{}, client *m
 	return erro
 }
 
-func RetornarTodos(ctx context.Context, nomeDB string,
+func (dbc *DBCommon) retornarTodos(ctx context.Context, nomeDB string,
 	nomeColecao string, modelo interface{}, client *mongo.Client, filtro bson.M) (interface{}, error) {
 
 	collection := client.Database(nomeDB).Collection(nomeColecao)
--- go.mod
+++ go.mod
@@ -3,14 +3,8 @@ module github.com/eduardohitek/golang-mongo-common
 go 1.12
 
 require (
-	github.com/go-stack/stack v1.8.0 // indirect
-	github.com/golang/snappy v0.0.1 // indirect
-	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
 	github.com/xdg/stringprep v1.0.0 // indirect
-	go.mongodb.org/mongo-driver v1.3.3
-	golang.org/x/crypto v0.0.0-20200602180216-279210d13fed // indirect
-	golang.org/x/net v0.0.0-20200602114024-627f9648deb9 // indirect
-	golang.org/x/sys v0.0.0-20200602225109-6fdc65e7d980 // indirect
-	golang.org/x/text v0.3.2 // indirect
-	golang.org/x/tools v0.0.0-20200601175630-2caf76543d99 // indirect
+	go.mongodb.org/mongo-driver v1.3.4
+	golang.org/x/crypto v0.0.0-20200604202706-70a84ac30bf9 // indirect
+	golang.org/x/sync v0.0.0-20200317015054-43a5402ce75a // indirect
 )
