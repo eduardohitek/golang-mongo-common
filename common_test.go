e319e7f7ec36b4d45ca069b479a7c693fa3bd7b3
Eduardo Hitek <eduardohitek@gmail.com>
2021-06-15 20:40:01 -0300

    test: adicionando mais testes

--- common_test.go
+++ common_test.go
@@ -2,11 +2,22 @@ package common
 
 import (
 	"context"
+	"os"
 	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
+
+func TestMain(m *testing.M) {
+	setup()
+	code := m.Run()
+	shutdown()
+	os.Exit(code)
+}
 
 func Test_setClientOptions(t *testing.T) {
 	connectionURI := "mongodb://localhost"
@@ -45,5 +56,24 @@ func Test_returnClient(t *testing.T) {
 	assert.NotNil(t, client, "should not be nil")
 	err = client.Ping(context.TODO(), nil)
 	assert.Nil(t, err, "should be nil")
+}
 
+func Test_Total(t *testing.T) {
+	client := getClient("localhost", "common-test")
+	total, err := Total(client, "test", "test", bson.M{})
+	assert.Nil(t, err, "should be nil")
+	assert.Equal(t, int64(0), total)
+}
+
+func Test_InsertOne(t *testing.T) {
+	client := getClient("localhost", "common-test")
+	insertResult, err := InsertOne(client, "test", "test", bson.E{Key: "timestamp", Value: time.Now()})
+	assert.Nil(t, err, "should be nil")
+	assert.NotNil(t, insertResult, "should not be nil")
+
+}
+
+func getClient(connectionURI string, appName string) *mongo.Client {
+	client, _ := ReturnClient(connectionURI, appName)
+	return client
 }
